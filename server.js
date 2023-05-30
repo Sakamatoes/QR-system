37b2dfae84e6f95903a38269764fbe88d0cd2a67
Apple Juice <mallari23adrian@gmail.com>
2023-05-30 21:28:32 +0800

    can now extract names from xlsx

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const ejs = require('ejs');
 const qr = require('qrcode');
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "/uploads/");
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage: storage});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,36 +26,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/generate', upload.fields([{ name: 'spreadsheet', maxCount: 1 }, { name: 'template', maxCount: 1 }]), (req, res) => {
-    try {
-      const spreadsheetPath = req.files['spreadsheet'][0].path;
-      const templatePath = req.files['template'][0].path;
-  
-      const workbook = new ExcelJS.Workbook();
-      workbook.xlsx.readFile(spreadsheetPath);
-  
-      const worksheet = workbook.worksheets[0];
-      const names = worksheet.getColumn('A').values.slice(1); // Assuming the names start from row 3 in column A
-  
-      const templateContent = fs.readFileSync(templatePath, 'utf-8');
-  
-      const certificates = [];
-  
-      for (const name of names) {
-        const qrCode = qr.toDataURL(name.toString());
-  
-        const certificate = ejs.render(templateContent, { name, qrCode });
-        certificates.push(certificate);
-      }
-  
-      console.log('Certificates:', certificates);
-  
-      res.render('certificates', { certificates: certificates.join('') });
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
+app.post('/generate', upload.fields([{name: 'spreadsheet', maxCount: 1}, {name: 'template', maxCount: 1}]), (req, res) => {
+
+  const workbook = new ExcelJS.Workbook();
+    const spreadsheet = workbook.getWorksheet('spreadsheet');
+    console.log(workbook.spreadsheet[0]);
   });
+
+app.get("/test", async (req, res) => {
+  const rowsArray = [];
+  const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(__dirname + '/uploads/spreadsheet.xlsx').then(data => {
+      const worksheet = workbook.getWorksheet(1); // Assuming you want to access the first worksheet
+  
+    worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+      const rowData = [];
+      row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
+        rowData.push(cell.value);
+      });
+      rowsArray.push(rowData);
+    });
+    });
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
